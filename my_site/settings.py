b8d3ff17e2a85368c342c20e7ba802c06956f2a2
LucasHirotsu <hirotsulucas@hotmail.com>
2023-09-26 19:59:17 -0300

    Registro básico e Login de usuários.

--- my_site/settings.py
+++ my_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -108,11 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
-
 TIME_ZONE = 'America/Sao_Paulo'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
@@ -125,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/admin'
+LOGOUT_REDIRECT_URL = '/admin'
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+@login_required # controle de acesso usando o decorador de função
+def sobre(request):
+    return HttpResponse('Este é um app de enquete!')
+
+# controle de acesso usando o mecanismo de herança que torna o login obrigatório
+class QuestionDeleteView(LoginRequiredMixin, DeleteView):
+model = Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
